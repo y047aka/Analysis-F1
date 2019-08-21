40772f4b2d2c49df579e101f477d46bbb1babe21
戸塚孝高 <y047aka@totsukayoshitakanoMac-mini.local>
2019-08-22 01:10:35 +0900

    Add elapsed field

--- tests/AnalysisTest.elm
+++ tests/AnalysisTest.elm
@@ -32,35 +32,35 @@ all =
     describe "Analysis"
         [ describe "analysisDecoder"
             [ describe "Lap型のデコード"
-                [ test "文字列：{ \"lap\": 20, \"time\": 90.003 } をデコードすると、Lap型を返す" <|
+                [ test "文字列：{ \"lap\": 20, \"time\": 90.003, \"elapsed\": 0 } をデコードすると、Lap型を返す" <|
                     \() ->
-                        decodeString lapDecoder "{ \"lap\": 20, \"time\": 90.003 }"
+                        decodeString lapDecoder "{ \"lap\": 20, \"time\": 90.003, \"elapsed\": 0 }"
                             |> (\result ->
                                     case result of
                                         Ok lap ->
-                                            Expect.equal (Lap 20 90.003) lap
+                                            Expect.equal (Lap 20 90.003 0) lap
 
                                         Err error ->
-                                            Expect.equal (Lap 20 90.003) (Lap 0 0)
+                                            Expect.equal (Lap 20 90.003 0) (Lap 0 0 0)
                                )
                 ]
             ]
         , describe "personalBest"
             [ describe "ドライバーの自己ベストラップを検索する"
-                [ test "(List Lap)：[ Lap 1 100.0, Lap 2 20.0, Lap 3 30.0 ] を渡すと、Lap型：Lap 2 20.0 を返す" <|
+                [ test "(List Lap)：[ Lap 1 100.0 0, Lap 2 20.0 0, Lap 3 30.0 0 ] を渡すと、Lap型：Lap 2 20.0 0 を返す" <|
                     \() ->
-                        [ Lap 1 100.0, Lap 2 20.0, Lap 3 30.0 ]
+                        [ Lap 1 100.0 0, Lap 2 20.0 0, Lap 3 30.0 0 ]
                             |> personalBest
-                            |> Expect.equal (Lap 2 20.0)
+                            |> Expect.equal (Lap 2 20.0 0)
                 ]
             ]
         , describe "fastestLap"
             [ describe "各ドライバーの自己ベストから、最も早いラップを検索する"
-                [ test "(List (List Lap))：[ [ Lap 1 100.0 ], [ Lap 2 20.0 ], [ Lap 3 30.0 ] ] を渡すと、Lap型：Lap 2 20.0 を返す" <|
+                [ test "(List (List Lap))：[ [ Lap 1 100.0 0 ], [ Lap 2 20.0 0 ], [ Lap 3 30.0 0 ] ] を渡すと、Lap型：Lap 2 20.0 0 を返す" <|
                     \() ->
-                        [ [ Lap 1 100.0 ], [ Lap 2 20.0 ], [ Lap 3 30.0 ] ]
+                        [ [ Lap 1 100.0 0 ], [ Lap 2 20.0 0 ], [ Lap 3 30.0 0 ] ]
                             |> fastestLap
-                            |> Expect.equal (Lap 2 20.0)
+                            |> Expect.equal (Lap 2 20.0 0)
                 ]
             ]
         ]
