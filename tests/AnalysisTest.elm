d929619d055dceae89e9ffd0719a7f6906fa3b99
戸塚孝高 <y047aka@totsukayoshitakanoMac-mini.local>
2019-08-22 01:16:36 +0900

    Add elepsed field

--- tests/AnalysisTest.elm
+++ tests/AnalysisTest.elm
@@ -10,8 +10,8 @@ import Test exposing (Test, describe, test)
 {-
 
    - [ ] analysisDecoder
-       - [ ] Lap型のデコード
-           - [x] 文字列：{ \"lap\": 20, \"time\": 90.003 } をデコードすると、Lap型を返す
+       - [x] Lap型のデコード
+           - [x] 文字列：{ \"lap\": 20, \"time\": 90.003, \"elapsed\": 0 } をデコードすると、Lap型を返す
 
        - [ ] History型のデコード
 
@@ -19,11 +19,11 @@ import Test exposing (Test, describe, test)
 
    - [ ] personalBest
        - [x] ドライバーの自己ベストラップを検索する
-           - [x] (List Lap)：[ Lap 1 100.0, Lap 2 20.0, Lap 3 30.0 ] を渡すと、Lap型：Lap 2 20.0 を返す
+           - [x] (List Lap)：[ Lap 1 100.0　0, Lap 2 20.0 0, Lap 3 30.0 0 ] を渡すと、Lap型：Lap 2 20.0 を返す
 
    - [ ] fastestLap
        - [x] 各ドライバーの自己ベストから、最も早いラップを検索する
-           - [x] (List (List Lap))：[ [ Lap 1 100.0 ], [ Lap 2 20.0 ], [ Lap 3 30.0 ] ] を渡すと、Lap型：Lap 2 20.0 を返す
+           - [x] (List (List Lap))：[ [ Lap 1 100.0 0 ], [ Lap 2 20.0 0 ], [ Lap 3 30.0 0 ] ] を渡すと、Lap型：Lap 2 20.0 0 を返す
 -}
 
 
